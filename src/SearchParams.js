14086847f114cc2fffb57dcd2c00130ceea8bb4f
Günther von Häfen <>
2019-04-21 04:15:14 +0200

    Added Breed search functionality

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { ANIMALS } from 'petfinder-client';
+import pf, { ANIMALS } from 'petfinder-client';
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+})
 
 class SearchParams extends React.Component {
   constructor(props) {
@@ -8,23 +13,49 @@ class SearchParams extends React.Component {
     this.state = {
       location: 'Seattle, WA',
       animal: '',
-      breed: ''
+      breed: '',
+      breeds: []
     }
 
     this.handleLocationChange = this.handleLocationChange.bind(this);
-    this.handleAnimalChange = this.handleAnimalChange.bind(this)
+    this.handleAnimalChange = this.handleAnimalChange.bind(this);
+    this.handleBreedChange = this.handleBreedChange.bind(this);
   }
 
   handleLocationChange(event) {
     this.setState({
       location: event.target.value
-    })
+    });
   }
 
   handleAnimalChange(event) {
     this.setState({
-      animal: event.target.value
-    })
+      animal: event.target.value,
+      breed: ''
+    }, this.getBreeds);
+  }
+
+  handleBreedChange(event) {
+    this.setState({
+      breed: event.target.value
+    });
+  }
+
+  getBreeds() {
+    if (this.state.animal) {
+      petfinder.breed.list({ animal: this.state.animal })
+      .then(data => {
+        if (data.petfinder && data.petfinder.breeds && Array.isArray(data.petfinder.breeds.breed)) {
+          this.setState({
+            breeds: data.petfinder.breeds.breed
+          });
+        } else {
+          this.setState({ breeds: [] });
+        }
+      })
+    } else {
+      this.setState({ breeds: [] });
+    }
   }
 
   render() {
@@ -55,6 +86,24 @@ class SearchParams extends React.Component {
           ))}
           </select>
         </label>
+        <label htmlFor='breed'>
+          Breed
+          <select
+            id='breed'
+            value={this.state.breed}
+            onChange={this.handleBreedChange}
+            onBlur={this.handleBreedChange}
+            disabled={!this.state.breeds.length}
+          >
+            <option />
+            {this.state.breeds.map(breed => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
       </div>
     )
   }
